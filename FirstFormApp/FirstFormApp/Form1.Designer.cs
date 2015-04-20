f77b7497e47b7a28ec7cfce88d4b97d366ba300f
deepAlgo-jaju <muhammadjunaidiftikhar@gmail.com>
2015-04-19 20:04:06 -0700

    FirstWindowsAPP
    this is my first windows app and i have made a login form

---
+++ FirstFormApp/FirstFormApp/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace FirstFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bt_login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_user = new System.Windows.Forms.TextBox();
+            this.tb_pass = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.tb_pass);
+            this.groupBox1.Controls.Add(this.tb_user);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.bt_login);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Font = new System.Drawing.Font("Gill Sans MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.groupBox1.ImeMode = System.Windows.Forms.ImeMode.AlphaFull;
+            this.groupBox1.Location = new System.Drawing.Point(12, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 152);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(32, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enrollment No";
+            // 
+            // bt_login
+            // 
+            this.bt_login.BackColor = System.Drawing.SystemColors.Control;
+            this.bt_login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.bt_login.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.bt_login.FlatAppearance.BorderSize = 5;
+            this.bt_login.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt_login.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt_login.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_login.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bt_login.Location = new System.Drawing.Point(200, 122);
+            this.bt_login.Margin = new System.Windows.Forms.Padding(8);
+            this.bt_login.Name = "bt_login";
+            this.bt_login.Size = new System.Drawing.Size(62, 24);
+            this.bt_login.TabIndex = 0;
+            this.bt_login.Text = "Login";
+            this.bt_login.UseVisualStyleBackColor = false;
+            this.bt_login.Click += new System.EventHandler(this.bt_login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label2.Location = new System.Drawing.Point(56, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // tb_user
+            // 
+            this.tb_user.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.tb_user.BackColor = System.Drawing.SystemColors.Control;
+            this.tb_user.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_user.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_user.Location = new System.Drawing.Point(124, 50);
+            this.tb_user.MaxLength = 13;
+            this.tb_user.Name = "tb_user";
+            this.tb_user.Size = new System.Drawing.Size(113, 19);
+            this.tb_user.TabIndex = 3;
+            // 
+            // tb_pass
+            // 
+            this.tb_pass.BackColor = System.Drawing.SystemColors.Control;
+            this.tb_pass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_pass.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_pass.Location = new System.Drawing.Point(124, 79);
+            this.tb_pass.MaxLength = 8;
+            this.tb_pass.Name = "tb_pass";
+            this.tb_pass.Size = new System.Drawing.Size(113, 19);
+            this.tb_pass.TabIndex = 4;
+            this.tb_pass.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(292, 269);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bt_login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_pass;
+        private System.Windows.Forms.TextBox tb_user;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FirstFormApp/FirstFormApp/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_login_Click(object sender, EventArgs e)
+        {
+            if(tb_user.Text=="01-133122-134"&& tb_pass.Text == "25885225")
+            {
+                MessageBox.Show("Welcome User");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username or/and Password");
+            }
+        }
+    }
+}
